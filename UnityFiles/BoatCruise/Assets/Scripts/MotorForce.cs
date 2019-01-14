6f43a3fd77f7041cf284fdf8a1df1ff6b4e599d4
NerbzHub <nathan.nette@gmail.com>
2019-01-14 22:49:44 +1100

    Set up the motor to be working and it now moves the boat based on the force.

--- UnityFiles/BoatCruise/Assets/Scripts/MotorForce.cs
+++ UnityFiles/BoatCruise/Assets/Scripts/MotorForce.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class MotorForce : MonoBehaviour
 {
+    public GameObject boat = null;
+    public float forcePower = 0.0f;
+
+    private Rigidbody thisRb;
 
     // Use this for initialization
     void Start()
     {
-
+        thisRb = boat.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        thisRb.AddForce(-gameObject.transform.forward * Time.deltaTime * forcePower , ForceMode.Force);
     }
 }
