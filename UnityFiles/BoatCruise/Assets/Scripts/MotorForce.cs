a2e2834fdcf21262cda0d76be82fe7f39dfd2f9e
NerbzHub <nathan.nette@gmail.com>
2019-01-14 22:27:05 +1100

    Created the motorforce script to create the force from the motor to move the boat.

--- UnityFiles/BoatCruise/Assets/Scripts/MotorForce.cs
+++ UnityFiles/BoatCruise/Assets/Scripts/MotorForce.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterManager : MonoBehaviour
+public class MotorForce : MonoBehaviour
 {
-    public GameObject[,] waterArray;
 
     // Use this for initialization
     void Start()
     {
-        waterArray = new GameObject[3,3];
+
     }
 
     // Update is called once per frame
