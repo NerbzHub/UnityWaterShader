47a71c14e4304df6f695e876a67b38d1c05af972
NerbzHub <nathan.nette@gmail.com>
2019-01-14 23:26:24 +1100

    The boat control script changes the direction of the motor force but it is having some strange effect of warping the cube. I believe it's because of transform.rotate. I tried it as a quick fix but it didn't work. Rotating the motor correctly is next on the list.

---
+++ UnityFiles/BoatCruise/Assets/Scripts/BoatControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatControl : MonoBehaviour
+{
+    public GameObject motor = null;
+    public float rotAmount;
+
+    private MotorForce motorForce;
+    private Rigidbody boatRb;
+
+    // Use this for initialization
+    void Start()
+    {
+        motorForce = motor.GetComponent<MotorForce>();
+        boatRb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputCheck();
+    }
+
+    void InputCheck()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            motor.transform.Rotate(0, rotAmount, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            motor.transform.Rotate(0, -rotAmount, 0);
+        }
+    }
+}
